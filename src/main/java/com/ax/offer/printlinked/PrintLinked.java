e968a83aa1e0688eeeb93832e2a16e41849ab5f1
455916695 <455916695@qq.com>
2018-10-23 19:03:17 +0800

    剑指offer 反序打印链表   ：

---
+++ src/main/java/com/ax/offer/printlinked/PrintLinked.java
@@ -0,0 +1,64 @@
+package com.ax.offer.printlinked;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ *  从尾到头打印链表
+ *           题目描述
+ * 输入链表的第一个节点，从尾到头反过来打印出每个结点的值。
+ * */
+public class PrintLinked {
+
+    /**
+     *  方案一：
+     *      使用stack
+     * */
+    public  ArrayList<Integer> printLink(ListNode listNode){
+
+        Stack<Integer> stack = new Stack<>();
+
+        while (listNode != null) {
+            stack.push(listNode.val);
+            listNode = listNode.next;
+        }
+
+        ArrayList<Integer> arrayList = new ArrayList();
+        while ( !stack.isEmpty()){
+            arrayList.add(stack.pop());
+        }
+
+        return  arrayList;
+    }
+
+
+    /**
+     * 方案二：
+     *      递归
+     *
+     * */
+    public ArrayList<Integer> printLink2(ListNode listNode) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        if(listNode != null ) {
+            arrayList.addAll( printLink2(listNode.next));
+            arrayList.add(listNode.val);
+        }
+        return  arrayList;
+    }
+
+
+    /**
+     *  方案三：
+     *      使用头插法
+     * */
+    public ArrayList<Integer> printLink3(ListNode listNode) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        //TODO  未完待续
+
+        return  arrayList;
+    }
+
+
+}
